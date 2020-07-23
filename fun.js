5cf1ebbd46df7e29dfbd201a99ec1cf422a3f248
Chocolin <you@example.com>
2020-07-23 12:35:46 +0400

    initial commit

--- fun.js
+++ fun.js
@@ -38,5 +38,30 @@ Star(6)
     }
 
             console.log(str)
+            
+const star=function(x){
+   if(x===0){
+     return "";
+}	
+return "*"+star(x-1);
+};
+const space=function(x){
+	if(x===0){
+		return"";
+	} 
+	return " "+space(x-1);
+};
+ const mix=function(x,y){
+ 	if(y===undefined){
+ 		y=0;
+ 	}
+ 	if(x===0){
+     return"";
+ 	}
+ 	return mix(x-1,y+1)+"\n"+space(y)+star(2*x-1);
+ };
+
+ console.log(mix(5));
+
 
 
